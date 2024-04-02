6438cbd17ea315776f2b2d32dd1ee0efab863aed
Wangari-j <wangarijudy@gmail.com>
2024-04-02 21:59:53 +0300

    kibo

---
+++ kibo.py
@@ -0,0 +1,15 @@
+#print "programming 10 times"
+
+message = "programming"
+
+number_of_copies = 10
+
+print (message * number_of_copies)
+
+#assign values to variables
+
+name = "Wangari"
+
+hours_learnt = 3
+
+print(name,hours_learnt)
